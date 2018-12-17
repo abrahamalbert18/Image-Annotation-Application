4daf92200023399cd31aa79e1d7c58e5b5f4cfc5
Abraham Albert <abrahamalbert18@icloud.com>
2018-12-17 17:15:40 +1100

    Splits the indices for training, validation and testing datasets.

---
+++ SplitterFile.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 17 16:25:07 2018
+
+@author: Albert
+"""
+import pandas as pd
+
+df = pd.read_csv("./PreprocessedDataFiles/MergedPreprocessedDataFiles/MergedPreprocessedDrinkingDataLabels v2.csv")
+# originalDF = df
+df['index'] = range(0, len(df))
+
+splitRatio = [0.6,0.2,0.2]
+
+def indicesSplit(splitRatio):
+    train = df.sample(frac = splitRatio[0], random_state = 42)
+    val = df.drop(train.index).sample(frac = 0.5, random_state = 42)
+    test = df.drop(train.index.append(val.index))#.sample(frac = splitRatio[2])
+    
+    return train, val, test
+
+if __name__=="__main__":
+    train, val, test = indicesSplit(splitRatio)
